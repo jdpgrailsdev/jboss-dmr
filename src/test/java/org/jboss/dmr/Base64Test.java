54cf90b8166d5b6b62d9164aa0aed860b2508ece
Jonathan Pearlin <jdp.grails.dev@gmail.com>
2011-04-01 22:29:05 -0400

    Updated tests to account for all padding scenarios. Updated ignore list to exclude Maven 3 ~ directory.

--- src/test/java/org/jboss/dmr/Base64Test.java
+++ src/test/java/org/jboss/dmr/Base64Test.java
@@ -14,10 +14,18 @@ public class Base64Test {
             assertEquals("QQ==", Base64.encode("A".getBytes()));
             assertEquals("QUI=", Base64.encode("AB".getBytes()));
             assertEquals("QUJD", Base64.encode("ABC".getBytes()));
+            assertEquals("YFR3YXMgYnJpbGxpZywgYW5kIHRoZSBzbGl0aHkgdG92ZXMgRGlkIGd5cmUgYW5kIGdpbWJsZSBpbiB0aGUgd2FiZTo=", Base64.encode("`Twas brillig, and the slithy toves Did gyre and gimble in the wabe:".getBytes()));
+            assertEquals("YFR3YXMgYnJpbGxpZywgYW5kIHRoZSBzbGl0aHkgdG92ZXMgRGlkIGd5cmUgYW5kIGdpbWJsZSBpbiB0aGUgd2FiZQ==", Base64.encode("`Twas brillig, and the slithy toves Did gyre and gimble in the wabe".getBytes()));
+            assertEquals("YFR3YXMgYnJpbGxpZywgYW5kIHRoZSBzbGl0aHkgdG92ZXMgRGlkIGd5cmUgYW5kIGdpbWJsZSBpbiB0aGUgd2Fi", Base64.encode("`Twas brillig, and the slithy toves Did gyre and gimble in the wab".getBytes()));
+            assertEquals("YFR3YXMgYnJpbGxpZywgYW5kIHRoZSBzbGl0aHkgdG92ZXMgRGlkIGd5cmUgYW5kIGdpbWJsZSBpbiB0aGUgd2E=", Base64.encode("`Twas brillig, and the slithy toves Did gyre and gimble in the wa".getBytes()));
+            assertEquals("YnJpbGxpZyw=", Base64.encode("brillig,".getBytes()));
+            assertEquals("cmlsbGlnLA==", Base64.encode("rillig,".getBytes()));
+            assertEquals("aWxsaWcs", Base64.encode("illig,".getBytes()));
+            assertEquals("bGxpZyw=", Base64.encode("llig,".getBytes()));
             assertEquals("VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw==",
                     Base64.encode("The quick brown fox jumps over the lazy dog".getBytes()));
         } catch (final Exception e) {
-            fail("IOException not expected.");
+            fail("Exception not expected: " + e.getMessage());
         }
     }
 
@@ -28,10 +36,18 @@ public class Base64Test {
             assertEquals("A", new String(Base64.decode("QQ==")));
             assertEquals("AB", new String(Base64.decode("QUI=")));
             assertEquals("ABC", new String(Base64.decode("QUJD")));
+            assertEquals("`Twas brillig, and the slithy toves Did gyre and gimble in the wabe:", new String(Base64.decode("YFR3YXMgYnJpbGxpZywgYW5kIHRoZSBzbGl0aHkgdG92ZXMgRGlkIGd5cmUgYW5kIGdpbWJsZSBpbiB0aGUgd2FiZTo=")));
+            assertEquals("`Twas brillig, and the slithy toves Did gyre and gimble in the wabe", new String(Base64.decode("YFR3YXMgYnJpbGxpZywgYW5kIHRoZSBzbGl0aHkgdG92ZXMgRGlkIGd5cmUgYW5kIGdpbWJsZSBpbiB0aGUgd2FiZQ==")));
+            assertEquals("`Twas brillig, and the slithy toves Did gyre and gimble in the wab", new String(Base64.decode("YFR3YXMgYnJpbGxpZywgYW5kIHRoZSBzbGl0aHkgdG92ZXMgRGlkIGd5cmUgYW5kIGdpbWJsZSBpbiB0aGUgd2Fi")));
+            assertEquals("`Twas brillig, and the slithy toves Did gyre and gimble in the wa", new String(Base64.decode("YFR3YXMgYnJpbGxpZywgYW5kIHRoZSBzbGl0aHkgdG92ZXMgRGlkIGd5cmUgYW5kIGdpbWJsZSBpbiB0aGUgd2E=")));
+            assertEquals("brillig,", new String(Base64.decode("YnJpbGxpZyw=")));
+            assertEquals("rillig,", new String(Base64.decode("cmlsbGlnLA==")));
+            assertEquals("illig,", new String(Base64.decode("aWxsaWcs")));
+            assertEquals("llig,", new String(Base64.decode("bGxpZyw=")));
             assertEquals("The quick brown fox jumps over the lazy dog",
                     new String(Base64.decode("VGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw==")));
         } catch (final Exception e) {
-            fail("IOException not expected.");
+            fail("Exception not expected: " + e.getMessage());
         }
     }
 }
